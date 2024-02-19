f956992a7191400bf04d38db1ad00a251d519340
hanabi <71324677+3eyka@users.noreply.github.com>
2024-02-19 18:57:12 +0100

    Delete wadbot_packer.ps1

--- wadbot_packer.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/3eyka/wadbot2600/main/wadbot.ps1" -OutFile "wadbot"; Set-ItemProperty -Path "wadbot" -Name "IsReadOnly" -Value $false; .\wadbot sentinel
